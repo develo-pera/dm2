57e4c15fcc6d3d9fa2ee959daa02dd79e6978306
Petar Popovic <petarpnp@gmail.com>
2025-07-29 23:22:46 +0200

    Update copy

--- src/app/components/landing/hero.tsx
+++ src/app/components/landing/hero.tsx
@@ -6,11 +6,10 @@ const Hero = () => {
     <div className="bg-[url('/dm2bg.png')] bg-cover bg-top">
       <div className="max-w-7xl mx-auto p-5 py-32 md:py-50">
         <div className="p-5 bg-black rounded-xs inline-block mb-5">
-          <p className="text-2xl md:text-3xl font-black leading-[1.1] text-white">Već od 150€</p>
+          <p className="text-2xl md:text-3xl font-black leading-[1.1] text-white">Ulaganje u nekretnine već od 150€</p>
         </div>
         <h1 className="text-5xl md:text-7xl font-black leading-[1.1] max-w-2xl md:max-w-[1000px] text-white">
-          Uložite u nekretnine i uživajte u pasivnom
-          prihodu i do 30% godišnje
+          Investirajte u deo kvadrata i ostvarite pasivan prihod i do 30% godišnje
         </h1>
         <div className="flex flex-wrap gap-5 mt-10">
           <WaitlistDialog>
